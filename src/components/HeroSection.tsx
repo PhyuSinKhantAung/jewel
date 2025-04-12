2573e2c66019d8229b420b529e8f92493ef34207
PhyuSinKhantAung <rollyphyusinkhant7290@gmail.com>
2025-04-12 19:18:25 +0700

    fixed hero image

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export const HeroSection = () => {
   return (
-    <section>
+    <section className="flex justify-center">
       <Image
         src="/images/hero-image.png"
         alt="jewel"
-        className="w-full"
+        className="min-h-[15rem] md:w-full object-cover" // 95% of viewport width on mobile
         width={1440}
         height={420}
       />
